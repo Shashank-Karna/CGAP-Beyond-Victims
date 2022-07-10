3f4c8e72d7e6da1cf5956af1ed956735ac3386af
Shashank-Karna <shashukarna9@gmail.com>
2022-07-10 22:47:16 +0530

    Added submission page and modified db

--- cgap/models.py
+++ cgap/models.py
@@ -24,6 +24,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
